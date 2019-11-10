54b557e898ccac613b252d2a98183e9ed8920183
Vladimir Zhelvis <war1lok@yandex.ru>
2019-11-11 00:09:56 +0300

    refactor localeSwitcher

--- src/components/localeSwitcher.js
+++ src/components/localeSwitcher.js
@@ -1,34 +1,37 @@
 import React from "react";
 import { Link } from "gatsby";
 import { Location } from "@reach/router";
-import { LocaleContext } from "./layout";
 import locales from "../../config/i18n";
 
+const prefixList = Object.keys(locales).filter(el => !locales[el].default);
+
 const LocaleSwitcher = ({ target, ...props }) => {
-  const { locale } = React.useContext(LocaleContext);
 
-  const resolvePath = path => {
-    const isIndex = path === `/`;
-
-    if (locales[locale].default) {
-      return locales[target].default
-        ? `${path}`
-        : `/${locales[target].path}${isIndex ? `` : `${path}`}`;
+  // Both functions returns path string w/o first slash
+  // the cause of the solution is gatsby Link component issues 
+  // https://github.com/gatsbyjs/gatsby/issues/11243
+  
+  const getDefaultPath = path => {
+    let pathArr = path.split("/");
+    if (prefixList.includes(pathArr[1])) {
+      pathArr.splice(0, 2);
     } else {
-      let pathArr = path.split("/");
-      if (locales[target].default) {
-        pathArr.splice(1, 1);
-      } else {
-        pathArr[1] = target;
-      }
-      return `${pathArr.join("/")}`;
+      pathArr.splice(0, 1);
     }
+    return pathArr.join("/");
   };
+
+  const getTargetPath = path => {
+    const to = getDefaultPath(path)
+    const isIndex = to === ``
+    return locales[target].default ? to : `${target}${isIndex ? `` : `/${to}`}`
+  }
 
   return (
     <Location>
       {({ location }) => {
-        return <Link {...props} to={`${resolvePath(location.pathname)}`} />;
+        console.log(getTargetPath(location.pathname))
+        return <Link {...props} to={`/${getTargetPath(location.pathname)}`} />;
       }}
     </Location>
   );
